4caf5899a066607bfe75359758fab3b03a027fdc
ninamatyukhina <nina_matyu@mail.ru>
2026-05-24 15:48:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
 
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "👋 Привет! Я бот для отслеживания цен на Ozon.\n\n"
+        "📌 Команды:\n"
+        "/add — добавить товар\n"
+        "/list — мои товары\n"
+        "/delete <id> — удалить товар")
