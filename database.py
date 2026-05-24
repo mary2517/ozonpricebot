03db3f76770a5572eb167c43f49a48f6673c934a
sono.euge <ekmonastyrskaya@edu.hse.ru>
2026-05-24 16:13:31 +0300

    Update database.py
    + товары конкретного пользователя, все товары из бд

--- database.py
+++ database.py
@@ -2,66 +2,93 @@ import sqlite3
 from config import our_database
 
 # создаем таблицу для продуктов
-con = sqlite3.connect(our_database)
-cursor = con.cursor()
+def new_database():
+    con = sqlite3.connect(our_database)
+    cursor = con.cursor()
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS products (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        user_id INTEGER, 
-        url TEXT, 
-        name TEXT, 
-        first_price REAL, 
-        last_price REAL, 
-        barrier REAL, 
-        message INTEGER DEFAULT 0 
-    )
-""")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            user_id INTEGER, 
+            url TEXT, 
+            name TEXT, 
+            first_price REAL, 
+            last_price REAL, 
+            barrier INTEGER, 
+            message INTEGER DEFAULT 0 
+        )
+    """)
 
-con.commit()
-con.close()
+    con.commit()
+    con.close()
 
 """
-параметры в таблице:
+в таблице:
     1) уникальный id товара
     2) id пользователя
     3) ссылка на товар
     4) название товара
     5) цена на момент добавления товара
     6) последняя цена
-    7) порог цены (при достижении которого будет высылаться сообщение в боте)
-    8) отправка сообщения (0 - нет, 1 - да)
+    7) порог цены
+    8) отправка сообщения
 """
 
 
-def add_product(user_id, url, name, first_price, last_price, barrier):
+def add_product(user_id, url, name, price, barrier):
     con = sqlite3.connect(our_database)
     cursor = con.cursor()
 
     cursor.execute("INSERT INTO products (user_id, url, name, first_price, last_price, barrier) VALUES (?, ?, ?, ?, ?, ?)",
-              (user_id, url, name, first_price, last_price, barrier))
+              (user_id, url, name, price, barrier))
 
     con.commit()
     con.close()
 
 
-def update_price(id, new_price):
+def update_price(id, new_price, message):
     con = sqlite3.connect(our_database)
     cursor = con.cursor()
 
-    cursor.execute("UPDATE products SET last_price=? WHERE id=?",
-                   (new_price, id))
+    cursor.execute("UPDATE products SET last_price=? message=? WHERE id=?",
+                   (new_price, message, id))
 
     con.commit()
     con.close()
 
 
-def delete_product(id, user_id):
+def delete_product(product_id, user_id):
     con = sqlite3.connect(our_database)
     cursor = con.cursor()
 
     cursor.execute("DELETE FROM products WHERE id=? AND user_id=?",
-              (id, user_id))
+              (product_id, user_id))
 
     con.commit()
     con.close()
+
+
+# товары конкретного пользователя
+def users_products(user_id):
+    con = sqlite3.connect(our_database)
+    cursor = con.cursor()
+
+    cursor.execute("SELECT id, name, first_price, last_price, barrier FROM products WHERE user_id=?",
+                   (user_id))
+    users_prod = cursor.fetchall()
+
+    con.close()
+    return users_prod
+    
+
+# все товары из бд
+def all_products():
+    con = sqlite3.connect(our_database)
+    cursor = con.cursor()
+
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+
+    con.close()
+    return products
+
